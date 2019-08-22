ac14fc1f1fd2088bc13944323e870aee70b8148c
charbo23 <charbo233@qq.com>
2019-08-22 23:12:40 +0800

    接入antd

--- react-router-demo02/src/AppRouter.js
+++ react-router-demo02/src/AppRouter.js
@@ -1,45 +1,117 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Layout, Menu, Icon } from 'antd';
+import antStyle from './style/antd-style.module.scss';
 import Index from './pages/Index';
 import Video from './pages/Video';
 import Workplace from './pages/Workplace';
-function AppRouter() {
-    let routeConfig = [
-        { id: 1, path: '/', title: '博客首页', exact: true, activeExact: true, component: Index },
-        { id: 2, path: '/video', title: '视频教程', exact: false, activeExact: false, component: Video },
-        { id: 3, path: '/workplace', title: '职场技能', exact: false, activeExact: false, component: Workplace },
-    ]
-    return (
-        <Router>
-            <div className="container">
-                <div className="left-nav">
-                    <h1>一级导航</h1>
-                    <ul>
-                        {
-                            routeConfig.map((item) => {
+const { Header, Sider, Content } = Layout;
+const { SubMenu } = Menu;
+class AppRouter extends Component {
+    state = {
+        collapsed: false,
+        routeConfig: [
+            { id: 1, iconType: 'home', path: '/', title: '博客首页', exact: true, hasSub: false, component: Index },
+            { id: 2, iconType: 'video-camera', path: '/video', title: '视频教程', exact: true, hasSub: true, component: Video },
+            { id: 3, iconType: 'radar-chart', path: '/workplace', title: '职场技能', exact: true, hasSub: true, component: Workplace },
+        ]
+    };
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    };
+    render() {
+        return (
+            // <Router>
+            //     <div className="container">
+            //         <div className="left-nav">
+            //             <h1>一级导航</h1>
+            //             <ul>
+            //                 {
+            //                     this.state.routeConfig.map((item) => {
 
-                                return (
-                                    <li key={item.id}><NavLink exact={item.activeExact} activeClassName="active-nav" to={item.path}>{item.title}</NavLink></li>
-                                )
+            //                         return (
+            //                             <li key={item.id}><NavLink exact={item.activeExact} activeClassName="active-nav" to={item.path}>{item.title}</NavLink></li>
+            //                         )
 
-                            })
-                        }
-                    </ul>
-                </div>
-                <div className="right-main">
-                    {
-                        routeConfig.map((item) => {
+            //                     })
+            //                 }
+            //             </ul>
+            //         </div>
+            //         <div className="right-main">
+            //             {
+            //                 this.state.routeConfig.map((item) => {
 
-                            return (
-                                <Route key={item.id} path={item.path} exact={item.exact} component={item.component} />
-                            )
+            //                     return (
+            //                         <Route key={item.id} path={item.path} exact={item.exact} component={item.component} />
+            //                     )
 
-                        })
-                    }
-                </div>
-            </div>
-        </Router>
-    )
+            //                 })
+            //             }
+            //         </div>
+            //     </div>
+            // </Router>
+            <Router>
+
+
+                <Layout className={antStyle['ant-layout']}>
+                    <Sider className={antStyle['ant-sider']} trigger={null} collapsible collapsed={this.state.collapsed}>
+                        <div className={antStyle['logo']} />
+                        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} >
+                            {
+                                this.state.routeConfig.map((item) => {
+                                    if(item.hasSub){
+                                        return (
+                                        
+                                            <SubMenu
+                                                key={item.id}
+                                                title={
+                                                    <Fragment>
+                                                        <Icon type={item.iconType} />
+                                                        <span>{item.title}</span>
+                                                    </Fragment>
+                                                }
+                                            >
+                                                <Menu.Item key={'sub' + item.id}>
+                                                    <Link to={item.path}>{item.title}</Link>
+                                                </Menu.Item>
+                                            </SubMenu>
+                                        );
+                                    }else{
+                                        return (
+                                        
+                                            <Menu.Item key={item.id}>
+                                                    <Icon type={item.iconType} />
+                                                    <span><Link to={item.path} style={{display:'inline-block'}}>{item.title}</Link></span>
+                                            </Menu.Item>
+                                        );
+                                    }
+                                    
+
+                                })
+                            }
+
+                        </Menu>
+                    </Sider>
+                    <Layout className={antStyle['ant-main-layout']}>
+                        <Header className={antStyle['ant-layout-header']}>
+                            <Icon
+                                className={antStyle['trigger']}
+                                type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                                onClick={this.toggle}
+                            />
+                        </Header>
+                        <Content
+                            className={antStyle['ant-layout-content']}
+                        >
+                            Content
+                    </Content>
+                    </Layout>
+                </Layout>
+            </Router>
+        )
+    }
 }
 
 export default AppRouter;
